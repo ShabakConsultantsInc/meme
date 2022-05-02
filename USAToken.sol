45e3d0760941e6bb56a563e45a9b714c88876ed9
ShabakConsultantsInc <ShabakConsultants@outlook.com>
2022-05-02 14:00:55 -0700

    initial of custom ERC20 codename USAToken.sol

---
+++ USAToken.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/ERC20.sol)
+
+pragma solidity ^0.8.0;
+
+import "./ERC20.sol";
+import "./IERC20.sol";
+// import "./SafeMath.sol";
+import "./extensions/IERC20Metadata.sol";
+import "./Context.sol";
+
+// 2500000000000000000000000000 = 25*10**9 * 10**18
+contract USAToken is ERC20 {
+    using SafeMath for uint256;
+    constructor(
+        uint256 initialSupply
+    ) ERC20("USA Coin", "USAC"){
+        _mint(msg.sender, initialSupply);
+
+        /**
+        * @dev Returns the name of the token.
+        */
+        function name() public view virtual returns (string memory) {
+            return "USAToken Name";
+        }
+        // external_link
+        function external_link() public view virtual returns(string memory){
+            return "https://magicsea.io";
+        }
+        // description
+            function description() public view virtual returns(string memory){
+            return "USA Token description";
+        } 
+    }
+}
